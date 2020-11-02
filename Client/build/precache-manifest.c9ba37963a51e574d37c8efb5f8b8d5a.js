4123dd25a92a087f3c46941e745264092a4a1aeb
shivachit1 <shivat@metropolia.fi>
2020-11-02 11:04:56 +0200

    Server dir change

---
+++ Client/build/precache-manifest.c9ba37963a51e574d37c8efb5f8b8d5a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d328ad07c4eda0ebd1ae3d2f83b5c1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90b1095408155fe68756",
+    "url": "/static/css/main.0c6f1332.chunk.css"
+  },
+  {
+    "revision": "3bc2a0e368bd1a4599b2",
+    "url": "/static/js/2.99c5517d.chunk.js"
+  },
+  {
+    "revision": "c2d61fbc538f1a837d9ed661e8528018",
+    "url": "/static/js/2.99c5517d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b1095408155fe68756",
+    "url": "/static/js/main.1dc4ddc4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "3bceb5937f17334d6e1291c67ae601d9",
+    "url": "/static/media/avatar.3bceb593.png"
+  },
+  {
+    "revision": "f6bb400043c3f76c15fc540034fcbca5",
+    "url": "/static/media/google.f6bb4000.png"
+  },
+  {
+    "revision": "b777aff6def0b73ec6f6bdd52f511e1e",
+    "url": "/static/media/graph.b777aff6.png"
+  }
+]);
